4b20cdc258a141f8584e02847f4272a3c33ab02d
ctstanley <ctstanley1@hotmail.com>
2015-06-15 16:27:52 -0700

    created picture param

--- project_vagabond_app/app/controllers/users_controller.rb
+++ project_vagabond_app/app/controllers/users_controller.rb
@@ -29,6 +29,6 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city)
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city, :picture)
 	end
 end
--- project_vagabond_app/db/schema.rb
+++ project_vagabond_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615174323) do
+ActiveRecord::Schema.define(version: 20150615230610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150615174323) do
     t.string   "current_city"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "picture"
   end
 
 end
