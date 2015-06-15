8ab04f44eaaf8e6c148d006e7cc6c372ba298d88
DAIMEON PILCHER <daimeon.pilcher@gmail.com>
2015-06-15 14:04:04 -0700

    added user sign up and verified functionality

--- project_vagabond_app/config/routes.rb
+++ project_vagabond_app/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 
-  get 'signup', to: 'user#new'
+  get 'signup', to: 'users#new'
+
+  post 'users', to: 'users#create'
+
+  get 'users/:id', to: 'users#show'
+
 end
