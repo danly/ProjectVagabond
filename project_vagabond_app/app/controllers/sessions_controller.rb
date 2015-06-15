5986624cbd70582743466c5fc78dac816ab90104
DAIMEON PILCHER <daimeon.pilcher@gmail.com>
2015-06-15 14:23:11 -0700

    defined create and destroy methods on session

--- project_vagabond_app/app/controllers/sessions_controller.rb
+++ project_vagabond_app/app/controllers/sessions_controller.rb
@@ -6,11 +6,21 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		
+		@user = User.confirm(user_params)
+		if @user
+			login(@user)
+			redirect_to "/users/#{@user.id}"
+		else
+			redirect_to login_path #add a flash message later
+		end
 	end
 
 	def destroy
-
+		logout
 	end
-
+	
+	private
+	def user_params
+		params.require(:user).permit(:email, :password)
+	end
 end
