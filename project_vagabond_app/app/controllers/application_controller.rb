96b59d644aed5b7fe349e1d9f1517105329c7156
ctstanley <ctstanley1@hotmail.com>
2015-06-18 10:18:50 -0700

    pull commit

--- project_vagabond_app/app/controllers/application_controller.rb
+++ project_vagabond_app/app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   include SessionsHelper
   protect_from_forgery with: :exception
+  def flash_class(level)
+  	case level
+  	when :notice then "alert alert-info"
+  	when :success then "alert alert-success"
+  	when :error then "alert alert-error"
+  	when :alert then "alert alert-error"
+  	end
+  end
 end
